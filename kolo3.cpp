b483597f4e573e1fca75575aaa0142ac220bec84
tunk <tunk@tunk>
2011-06-09 10:11:30 +0200

    dodana klasa Kolo, liczy sie pole kola

--- kolo3.cpp
+++ kolo3.cpp
@@ -3,7 +3,17 @@
 #include <iostream>
 using namespace std;
 
+class Kolo {
+  public:
+    Kolo(float r=1.0) : r_(r) {}
+    float pole() { return 3.14159*r_*r_; }
+  private:
+    float r_;
+};
+
 int main(int argc, char **argv) {
+  Kolo k(2.5);
+  cout<<"Pole kola o promieniu 2.5 wynosi "<<k.pole()<<endl;
   return 0;
 }
 
