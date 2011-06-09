025f656aa8e895afee5270b35833c2a80705e9c6
tunk <tunk@tunk>
2011-06-09 10:23:42 +0200

    pobieranie informacji o studencie zapisywane do pliku

--- kolo3.cpp
+++ kolo3.cpp
@@ -1,10 +1,23 @@
 // Imie nazwisko, grupa(specjalnosc), nr. indeksu
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, char **argv) {
   cout<<"Program pobiera informacje od studenta i zapisuje je do pliku"<<endl;
+
+  ofstream plik("student-info.txt");
+  string imie, nazwisko;
+  int nr_indeksu;
+
+  cout<<"imie: "; cin>>imie;
+  cout<<"nazwisko: "; cin>>nazwisko;
+  cout<<"nr. indeksu: "; cin>>nr_indeksu;
+
+  plik<<imie<<" "<<nazwisko<<", "<<nr_indeksu<<endl;
+  plik.close();
   return 0;
 }
 
