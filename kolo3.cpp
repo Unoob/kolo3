e7ef4d468dfe1558147ec33344d0bd2da0cdda4d
unknown <nowoczki@interia.pl>
2011-06-10 11:37:34 +0200

    zatwierdzenie

--- kolo3.cpp
+++ kolo3.cpp
@@ -1,5 +1,5 @@
 // Imie nazwisko, grupa(specjalnosc), nr. indeksu
-
+// Konrad Kwiecinski, zsziad, 101512
 #include <iostream>
 using namespace std;
 
@@ -13,7 +13,7 @@ class Kolo {
 
 int main(int argc, char **argv) {
   Kolo k(2.5);
-  cout<<"Pole kola o promieniu 2.5 wynosi "<<k.pole()<<endl;
+  cout<<"Pole kola o promieniu 2.5 wynosi "<<k.pole()<<endl;//wynik: ~19,63
   return 0;
 }
 
